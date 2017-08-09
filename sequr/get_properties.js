7fccbbc02c105367c467034355da1de6e000d051
David Gatti <github@gatti.pl>
2017-08-09 19:05:47 +0200

    Gettign the properties #3

--- sequr/get_properties.js
+++ sequr/get_properties.js
@@ -5,27 +5,71 @@ module.exports = function(container) {
 	return new Promise(function(resolve, reject) {
 
 		//
-		//
+		//	1.	Prepare the request
 		//
 		let option = {
-			url: "https://google.com",
+			url: "https://api.sequr.io/v1/user/me/properties",
+			json: true,
+			headers: {
+				Authorization: "Bearer " + container.sequr_api_key
+			}
 		}
 
 		//
-		//  5.	Make a request to the Auth server to validate the token
+		//  2.	Make the request to get all the user properties
 		//
-		request.post(option, function(error, response, body) {
+		request.get(option, function(error, response, body) {
 
+			//
+			//	1.	Check if there were no internal errors
+			//
 			if(error)
 			{
 				return reject(error)
 			}
 
-			container.properties = ['Green House', 'Mountain lake', 'Park On the Hill', 'Peache and quiet'];
+			//
+			//	2. Check if the server didn't have any issues
+			//
+			if(response.statusCode >= 300)
+			{
+				return reject(new Error(body.message))
+			}
 
+			//
+			//	3.	Make an empty array where to store all the user properties
+			//
+			let properties = [];
+
+			//
+			//	4.	Loop over the result and save all the properties names
+			//		in to the array
+			//
+			body.data.forEach(function(data) {
+
+				//
+				//	1.	Extract the name of the property
+				//
+				let name = data.property.name;
+
+				//
+				//	2.	Add the name to the array
+				//
+				properties.push(name);
+
+			});
+
+			//
+			//	5.	Save the properties for other promises to use
+			//
+			container.properties = properties;
+
+			//
+			//	->	Move to the next chain
+			//
 			return resolve(container);
 
-		})
+		});
 
 	});
 
